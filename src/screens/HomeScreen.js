4cb4f26236d8f072be6628fe1b7269fd56529fc5
mzfais <mzfais@gmail.com>
2023-11-04 07:52:07 +0700

    downgrade tailwindcss to 3.2.2 to fix error use process(css).then(cb) to work with async plugins

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,8 +3,10 @@ import React from 'react';
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
-    </SafeAreaView>
+    <View className="flex-1 bg-neutral-800">
+      <SafeAreaView className="-mb-2">
+        <Text>HomeScreen</Text>
+      </SafeAreaView>
+    </View>
   );
 }
