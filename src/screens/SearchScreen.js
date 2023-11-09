c740b3500a4965111e907c12733a4fef04b2b2f8
mzfais <mzfais@gmail.com>
2023-11-09 20:19:28 +0700

    fix search screen ui

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,10 +1,78 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  Dimensions,
+  TouchableOpacity,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Image,
+} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {useNavigation} from '@react-navigation/native';
+import {XMarkIcon} from 'react-native-heroicons/outline';
+
+const {width, height} = Dimensions.get('window');
 
 export default function SearchScreen() {
+  const navigation = useNavigation();
+  const [results, setResults] = useState([1, 2, 3, 4]);
+  let movieName = 'Equalizer 3';
   return (
-    <View>
-      <Text>SearchScreen</Text>
-    </View>
+    <SafeAreaView className="bg-neutral-800 flex-1">
+      <View className="mx-4 mb-3 flex-row justify-between items-center border border-neutral-500 rounded-full">
+        <TextInput
+          placeholder="Cari Film"
+          placeholderTextColor={'lightgray'}
+          className="pb-1 pl-6 flex-1 text-base font-semibold text-white tracking-wider"
+        />
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Home')}
+          className="rounded-full p-3 m-1 bg-neutral-500">
+          <XMarkIcon size={25} color="white" />
+        </TouchableOpacity>
+      </View>
+      {/* hasil pencarian */}
+      {results.length > 0 ? (
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{paddingHorizontal: 15}}
+          className="space-y-3">
+          <Text className="text-white font-semibold ml-1">
+            Results ({results.length})
+          </Text>
+          <View className="flex-row justify-between flex-wrap">
+            {results.map((item, index) => {
+              return (
+                <TouchableWithoutFeedback
+                  key={index}
+                  onPress={() => navigation.push('Movie', item)}>
+                  <View className="space-y-2 mb-4">
+                    <Image
+                      className="rounded-3xl"
+                      source={require('../../assets/images/moviePoster2.jpeg')}
+                      style={{width: width * 0.44, height: height * 0.3}}
+                    />
+                    <Text className="text-gray-300 ml-1">
+                      {movieName.length > 22
+                        ? movieName.slice(0, 22) + '...'
+                        : movieName}
+                    </Text>
+                  </View>
+                </TouchableWithoutFeedback>
+              );
+            })}
+          </View>
+        </ScrollView>
+      ) : (
+        <View className="flex-row justify-center">
+          <Image
+            source={require('../../assets/images/movieTime.png')}
+            className="h-96 w-96"
+          />
+        </View>
+      )}
+    </SafeAreaView>
   );
 }
