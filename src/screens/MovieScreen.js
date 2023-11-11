57697105a92b8a03dba329ea550680de0bfa0988
mzfais <mzfais@gmail.com>
2023-11-11 08:38:27 +0700

    add all movie details info to loading state

--- src/screens/MovieScreen.js
+++ src/screens/MovieScreen.js
@@ -73,85 +73,91 @@ export default function MovieScreen() {
       contentContainerStyle={{paddingBottom: 20}}
       className="flex-1 bg-neutral-900">
       {/* back button dan poster */}
-      <View className="w-full">
-        <SafeAreaView
-          className={
-            'absolute z-20 w-full flex-row justify-between items-center px-4' +
-            topMargin
-          }>
-          <TouchableOpacity
-            style={styles.background}
-            className="rounded-xl p-1"
-            onPress={() => navigation.goBack()}>
-            <ChevronLeftIcon size={28} strokeWidth={2.5} color="white" />
-          </TouchableOpacity>
-          <TouchableOpacity onPress={() => toggleFavorite(!isFavorite)}>
-            <HeartIcon
-              size={35}
-              color={isFavorite ? theme.background : 'white'}
-            />
-          </TouchableOpacity>
-        </SafeAreaView>
-        {loading ? (
-          <Loading />
-        ) : (
-          <View>
-            <Image
-              // source={require('../../assets/images/moviePoster1.jpeg')}
-              source={{
-                uri: image500(movie?.poster_path) || fallbackMoviePoster,
-              }}
-              style={{
-                width,
-                height: height * 0.55,
-              }}
-            />
-            <LinearGradient
-              colors={[
-                'transparent',
-                'rgba(23, 23, 23, 0.8)',
-                'rgba(23, 23, 23, 1)',
-              ]}
-              style={{width, height: height * 0.4}}
-              start={{x: 0.5, y: 0}}
-              end={{x: 0.5, y: 1}}
-              className="absolute bottom-0"
-            />
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <View className="w-full">
+            <SafeAreaView
+              className={
+                'absolute z-20 w-full flex-row justify-between items-center px-4' +
+                topMargin
+              }>
+              <TouchableOpacity
+                style={styles.background}
+                className="rounded-xl p-1"
+                onPress={() => navigation.goBack()}>
+                <ChevronLeftIcon size={28} strokeWidth={2.5} color="white" />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => toggleFavorite(!isFavorite)}>
+                <HeartIcon
+                  size={35}
+                  color={isFavorite ? theme.background : 'white'}
+                />
+              </TouchableOpacity>
+            </SafeAreaView>
+            <View>
+              <Image
+                // source={require('../../assets/images/moviePoster1.jpeg')}
+                source={{
+                  uri: image500(movie?.poster_path) || fallbackMoviePoster,
+                }}
+                style={{
+                  width,
+                  height: height * 0.55,
+                }}
+              />
+              <LinearGradient
+                colors={[
+                  'transparent',
+                  'rgba(23, 23, 23, 0.8)',
+                  'rgba(23, 23, 23, 1)',
+                ]}
+                style={{width, height: height * 0.4}}
+                start={{x: 0.5, y: 0}}
+                end={{x: 0.5, y: 1}}
+                className="absolute bottom-0"
+              />
+            </View>
           </View>
-        )}
-      </View>
-      {/* detil film */}
-      <View style={{marginTop: -(height * 0.09)}} className="space-y-3">
-        {/* title */}
-        <Text className="text-white text-center text-3xl font-bold tracking-wider">
-          {movie?.title}
-        </Text>
-        {/* status, release, runtime */}
-        <Text className="text-neutral-400 text-center text-base font-semibold">
-          {movie?.status} • {movie?.release_date?.split('-')[0]} •{' '}
-          {movie?.runtime} min
-        </Text>
-        {/* genre */}
-        <View className="flex-row justify-center mx-4 space-x-2">
-          {movie?.genres?.map((genre, index) => {
-            let showDot = index + 1 !== movie.genres.length;
-            return (
-              <Text
-                key={'genre-' + index}
-                className="text-neutral-400 text-center text-base font-semibold">
-                {genre?.name} {showDot ? '•' : null}
-              </Text>
-            );
-          })}
-        </View>
-        {/* deskripsi */}
-        <Text className="text-neutral-400 mx-4 tracking-wide">
-          {movie?.overview}
-        </Text>
-      </View>
-      {/* Pemeran */}
-      <Cast navigation={navigation} cast={cast} />
-      <MovieList title="Film Serupa" hideSeeAll={true} data={similarMovies} />
+          {/* detil film */}
+          <View style={{marginTop: -(height * 0.09)}} className="space-y-3">
+            {/* title */}
+            <Text className="text-white text-center text-3xl font-bold tracking-wider">
+              {movie?.title}
+            </Text>
+            {/* status, release, runtime */}
+            <Text className="text-neutral-400 text-center text-base font-semibold">
+              {movie?.status} • {movie?.release_date?.split('-')[0]} •{' '}
+              {movie?.runtime} min
+            </Text>
+            {/* genre */}
+            <View className="flex-row justify-center mx-4 space-x-2">
+              {movie?.genres?.map((genre, index) => {
+                let showDot = index + 1 !== movie.genres.length;
+                return (
+                  <Text
+                    key={'genre-' + index}
+                    className="text-neutral-400 text-center text-base font-semibold">
+                    {genre?.name} {showDot ? '•' : null}
+                  </Text>
+                );
+              })}
+            </View>
+            {/* deskripsi */}
+            <Text className="text-neutral-400 mx-4 tracking-wide">
+              {movie?.overview}
+            </Text>
+          </View>
+          {/* Pemeran */}
+          <Cast navigation={navigation} cast={cast} />
+          <MovieList
+            title="Film Serupa"
+            hideSeeAll={true}
+            data={similarMovies}
+          />
+        </>
+      )}
     </ScrollView>
   );
 }
