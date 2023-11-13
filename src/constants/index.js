aa3784b25e758ca910891d571af15d8ec576f3c6
mzfais <mzfais@gmail.com>
2023-11-13 21:44:04 +0700

    use your own api key

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 const baseUrl = 'https://api.themoviedb.org/3/';
 const posterImageUrl = 'https://image.tmdb.org/t/p/';
-const apiKey = 'e4ca67f9bd2d9c0969f69cad7352315a';
+const apiKey = 'use-your-own-key';
 
 export {baseUrl, posterImageUrl, apiKey};
